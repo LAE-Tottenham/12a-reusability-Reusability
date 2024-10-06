4b3cee5365659a225ba1daf0878b04f2d8480552
Uzma-Rashid <Uzma.Rashid@laetottenham.ac.uk>
2024-10-06 15:06:21 +0000

    Pending changes exported from your codespace

--- Task1.py
+++ Task1.py
@@ -1,46 +1,67 @@
 # Help! My code is too messy :( Please help me organise it and extract out the duplications.
 
 # Define your reusable functions here:
+#triangle_sides(), weird_calculation(), hyptenuse_calculation()
+
 # Make sure each function only does ONE thing!!!!!!!!!!!
 
 
 
 ###########################################
+import math
+def triangle_sides(triangle_num):
+    while True:
+        try:
+            opp1 = float(input("Enter your triangle's opposite side length: "))
+            if opp1 > 0:
+                adj1 = float(input("Enter your triangle's adjacent side length: "))
+                break
+            else:
+                print("Try again, you must enter a positive value")
+        except ValueError:
+            print("Enter a numeric value please")
+    return opp1,adj1
+
+def hypotenuse_calculation(opp1,adj1):
+    return math.sqrt(opp1**2 + adj1**2)
 
 def weird_calculation():
-    # get the length and width of the first triangle from the user
-    opp1 = float(input("Enter your first triangle's opposite side length: "))
-    adj1 = float(input("Enter your first triangle's adjacent side length: "))
+    opp1,adj1 = triangle_sides(1)
+    hyp1 = hypotenuse_calculation(opp1,adj1)
 
-    # work out the hyp
-    import math
-    hyp1 = math.sqrt(opp1**2 + adj1**2)
+    opp2,adj2 = triangle_sides(2)
+    hyp2 = hypotenuse_calculation(opp2,adj2)
 
-    # get the length and width of the second triangle from the user
-    opp2 = float(input("Enter your second triangle's opposite side length: "))
-    adj2 = float(input("Enter your second triangle's adjacent side length: "))
-
-    # work out the hyp
-    import math
-    hyp2 = math.sqrt(opp2**2 + adj2**2)
-
-    # create a third triangle with the hyp1 as the opp and hyp2 as the adj
-    opp3 = hyp1
-    adj3 = hyp2
-    
-    import math
-    hyp3 = math.sqrt(opp3**2 + adj3**2)
+    hyp3 = hypotenuse_calculation(hyp1,hyp2)
+   
     return hyp3
 
-weird_answer = weird_calculation()
-print(weird_answer)
+print(weird_calculation())
 
 
 # After you have written the reusable functions, answer the following:
 # Questions:
 # 1. What are the preconditions for your code not to break?
+# The lengths must be positive, the total of 2 sides of the triangle must be greater that the third side.
+
 # 2. Validate the user's input based on your preconditions.
+#def triangle_sides(triangle_num):
+    #while True:
+     #   try:
+      #      opp1 = float(input("Enter your triangle's opposite side length: "))
+       #     if opp1 > 0:
+        #        adj1 = float(input("Enter your triangle's adjacent side length: "))
+         #       break
+          #  else:
+               # print("Try again, you must enter a positive value")
+       # except ValueError:
+           # print("Enter a numeric value please")
+   # return opp1,adj1
+#
 # 3. Why was it useful to use reusable components in this case? Please mention at least 2 reasons and don't forget to contextualise.
+# It was useful as i didn't need to rewrite the code for the hypotenuse calculation, so it saved me time.
+# Also, my code was shorter as i was able to use only 1 function to calculate the hypotenuse of 2 triangles.
+
 
 # Further Tasks:
 # 1. Put your functions in seperate appropriate files and import them in.
--- Task2.py
+++ Task2.py
@@ -5,6 +5,7 @@ import os, time
 # Help! I'm trying to make this cool bot but my code is too messy :( Please help me organise it into reusable components.
 
 # Define your reusable functions here:
+#guess_gender, postcode_information,weather,get_cat_fact,message,weird_weather_bot
 # Make sure each function only does ONE thing!!!!!!!!!!!
 
 def guess_gender(name):
@@ -15,44 +16,60 @@ def guess_gender(name):
 
 ###########################################
 
-def weird_weather_bot():
-    
+def postcode_information(postcode):
+    postcode_resp = requests.get(f"https://api.postcodes.io/postcodes/{postcode_raw}").json()
+    result = postcode_resp['result']
+    return result['admin_ward'], result['longtitude'], result['latitude']
+
+def weather(latitude,longtitude):
+    weather_resp = requests.get(f"https://api.openweathermap.org/data/2.5/weather?lat={latitude}&lon={longitude}&appid=4d30afa58f6f935d861edecad3639cda").json()
+    kelvin = weather_resp["main"]["temp"]
+    degrees = int(kelvin - 273.15)
+    main_weather = weather_resp["weather"][0]["main"]
+    second_weather = weather_resp["weather"][0]["description"]
+    return degrees, main_weather, second_weather
+
+def  get_cat_fact():
+    joke_resp = requests.get("https://catfact.ninja/fact").json()
+    return joke_resp['fact']
+
+def message():
     f = Figlet(font="slant")
     print(f.renderText("HEY!"))
 
     print("Welcome to the weird weather bot :)")
     print("-----------------------------------\n")
+
+def weird_weather_bot():
+    message()
     name = input("May I take your first name please? ")
-    gender_result = guess_gender(name)
-    gender = gender_result[0]
-    prob_percent = gender_result[1]
+    gender, prob_percent = guess_gender(name)
     print(f"\nHmmm, I'm {prob_percent}% sure you are a {gender}.")
-    
     gender_correct = input("Am I right? :) (Y/n)")
     if gender_correct.lower() in ["", "yes", "y", "ye"]:
         print("Wooooooh! Computer 1, Human 0.")
     else:
         print("Ahhhh, sorry! :(")
-
-    postcode_raw = input("\nSo, what's your postcode? ")
-    postcode_resp = requests.get(f"https://api.postcodes.io/postcodes/{postcode_raw}").json()
-
-    area = postcode_resp['result']['admin_ward']
-    longitude = postcode_resp['result']['longitude']
-    latitude = postcode_resp['result']['latitude']
-    print(f"Nice! so you live in {area}.\n")
-
-    print("Let me just check the weather there today...\n")
     
+    postcode_raw = input("/nso, what's your postcode? ")
+    area,longtitude,latitude = postcode_information(postcode_raw)
+    print(f"Nice! so you live in {area}.\n")
+    print("Let me just check the weather there today...\n")
+
     for i in range(3):
         time.sleep(1)
         print("...")
+    def yes_no():
+        answer = input("\nWould you like a cat fact while you wait? ").strip().lower()
+        if answer in ['yes', 'no']:
+            return answer
+        else:
+            print("Please enter yes or no ")
+
     
-    input("\nWould you like a cat fact while you wait? ")
     print("Doesn't matter what you think, I'm going to give you one anyway :)")
     time.sleep(3)
-    joke_resp = requests.get("https://catfact.ninja/fact").json()
-    joke = joke_resp['fact']
+    joke=get_cat_fact()
     print("\n###########################")
     print("CAT FACT:")
     print(f"\n{joke}\n")
@@ -67,26 +84,34 @@ def weird_weather_bot():
         time.sleep(1)
         print("...")
 
-    weather_resp = requests.get(f"https://api.openweathermap.org/data/2.5/weather?lat={latitude}&lon={longitude}&appid=4d30afa58f6f935d861edecad3639cda").json()
-    # print(weather_resp)
-    weather_kelvin = weather_resp["main"]["temp"]
-    # convert to degrees
-    weather_degrees = int(weather_kelvin - 273.15)
-    main_weather_desc = weather_resp["weather"][0]["main"]
-    second_weather_desc = weather_resp["weather"][0]["description"]
+    degrees, main_weather, second_weather = weather(latitude,longtitude)
     print(f"\nThe weather in {area}:\n")
     print(str(weather_degrees) + "℃")
     print(f"{main_weather_desc} - {second_weather_desc}")
     print("\nThank you! Bye.")
 
-weird_weather_bot()
+    if name == "main":
+        main()
+
 
 
 # After you have written the reusable functions, answer the following:
 # Questions:
 # 1. What are the preconditions for your code not to break?
+#The user's answer to whether they want a cat fact or not should only be yes or no
+
 # 2. Validate the user's input based on your preconditions.
+#def yes_no():
+        #answer = input("\nWould you like a cat fact while you wait? ").strip().lower()
+        #if answer in ['yes', 'no']:
+        #   return answer
+        #else:
+        #  print("Please enter yes or no ")
+
 # 3. Why was it useful to use reusable components in this case? Please mention at least 2 reasons and don't forget to contextualise.
+# It was useful to use reusable components as it was easier to understand the code as the functions, guess_gender, postcode_information,weather,get_cat_fact,message,weird_weather_bot all do different things
+#Also, I was able to use the functions i created multiple times
+
 
 # Further Tasks:
 # 1. Put your functions in seperate appropriate files and import them in.
